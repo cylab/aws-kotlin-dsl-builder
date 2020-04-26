464669121b121653cbbe9a6fdd2e5311945d823b
Mathias Henze <mhenze@jeto.de>
2020-04-26 10:43:22 +0200

    made builder public to allow using it as workaround for missing sub-dsls

--- src/main/kotlin/net/highteq/awssdk/awskotlindslbuilder/target/templates.kt
+++ src/main/kotlin/net/highteq/awssdk/awskotlindslbuilder/target/templates.kt
@@ -80,7 +80,7 @@ fun typeDSL(model: TypeDSLModel) =
   ${annotations(model.annotations)}
   class ${model.name} {
     @Deprecated("Usage of the builder field is not recommended. It might vanish in any new release!", level = WARNING)
-    internal val builder = ${model.targetType}.builder()
+    val builder = ${model.targetType}.builder()
     internal fun build(): ${model.targetType} = builder.build()
     ${dslProperties(model.dslProperties)}
     ${dslFunctions(model.dslFunctions)}
